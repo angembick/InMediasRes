a5ac488da1241c438145453a9ccc2eb9a8935936
Angelina Wedemeyer <angelina.wedemeyer@gmail.com>
2014-07-27 14:18:05 -0700

    test

---
+++ flight/flight-plan.php
@@ -0,0 +1,30 @@
+<!DOCTYPE>
+<html>
+<head>
+
+</head>
+<body>
+	<div>
+		<!--<img src="flight/image.jpg">
+	--></div>
+	<?php
+
+$path = phpinfo();
+
+?>
+
+
+
+<?php echo " the path is " .$path; ?>
+<p>hi</p>
+
+      <?php
+        echo 7-4 == 3;
+      ?>
+<?php
+
+phpinfo();
+
+?>
+</body>
+</html>
