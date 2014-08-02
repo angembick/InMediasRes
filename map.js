d0e3abdf4b57a26c0cae5a1f97c3ff1b051b1600
Angelina Wedemeyer <angelina.wedemeyer@gmail.com>
2014-08-02 09:31:04 -0700

    c

--- map.js
+++ map.js
@@ -5,9 +5,5 @@ function initialize() {
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
-        google.maps.event.addListener(map, 'idle', function()
-{
-    jQuery('.gm-style').removeClass('gm-style');
-});
       }
       google.maps.event.addDomListener(window, 'load', initialize);
