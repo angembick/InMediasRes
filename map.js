e59a53fd99b2919e8cda4164d395798b3498a5ed
Angelina Wedemeyer <angelina.wedemeyer@gmail.com>
2014-08-02 09:04:02 -0700

    separate

--- map.js
+++ map.js
@@ -1,7 +1,9 @@
-new google.maps.Map(document.getElementById("map"), {
-    zoom: 1,
-    disableDefaultUI: true,
-    zoomControl: true,
-    center: new google.maps.LatLng(28, -41),
-    mapTypeId: "roadmap"
-});
+function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(37.7423320, -122.4524330),
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
