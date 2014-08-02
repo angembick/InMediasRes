970fbce545c59f0ea303225d083c8303e983f134
Angelina Wedemeyer <angelina.wedemeyer@gmail.com>
2014-08-02 08:36:32 -0700

    test

---
+++ map.js
@@ -0,0 +1,7 @@
+new google.maps.Map(document.getElementById("map"), {
+    zoom: 1,
+    disableDefaultUI: true,
+    zoomControl: true,
+    center: new google.maps.LatLng(28, -41),
+    mapTypeId: "roadmap"
+});
