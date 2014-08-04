99a3531c054d991ccf5c6c3c761533ac9424cc58
Angelina Wedemeyer <angelina.wedemeyer@gmail.com>
2014-08-04 12:30:01 -0700

    z

--- enroute.js
+++ enroute.js
@@ -1,22 +1,4 @@
-var main = function(){
-    $('.icon-menu').click(function(){
-        $('.menu').animate({
-            left:'0px'
-            }, 200);
-        $('body').animate({
-            right: '285px'
-            }, 200);
-    });
-    
-    $('.icon-close').click(function(){
-        $('.menu').animate({
-            left: '-285px'
-            }, 200);
-        $('body').animate({
-            left: '0px'
-            }, 200);
-    });
-};
+
 
 function initialize() {
     var latlng = new google.maps.LatLng(-34.397, 150.644);
