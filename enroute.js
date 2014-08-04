2116b65a8a00c1af3589f85ba12b55bce384edfd
Angelina Wedemeyer <angelina.wedemeyer@gmail.com>
2014-08-04 12:15:33 -0700

    WEB

--- enroute.js
+++ enroute.js
@@ -19,13 +19,20 @@ var main = function(){
 };
 
 function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(37.742332037, -122.4524330),
-          zoom: 12
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);    
-    
-$(document). ready(main)
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var myOptions = {
+        zoom: 8,
+        center: latlng,
+        panControl: false,
+        zoomControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        myOptions);
+  }
+
+Y.on("domready", function(){
+     initialize();   
+})
