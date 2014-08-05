553348cce3ff12a55b77b0fe03747e4acea8a971
Angelina Wedemeyer <angelina.wedemeyer@gmail.com>
2014-08-05 16:27:39 -0700

    t

--- pictures.js
+++ pictures.js
@@ -7,11 +7,9 @@ var main = function() {
     url: "https://api.instagram.com/v1/media/popular?access_token=48121533.5db54f4.df09ebad5d3d47d3979743c9c3b57b37",
     success: function(data) {
       for (var i = 0; i < 6; i++) {
-        $(".popular").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></li>");
+        $(".latest").append("<li>hi</li>");
       }
     }
-for (var i = 0; i < 6; i++) {
-        $(".latest").append("<li>hi</li>");
 
   });
 }
