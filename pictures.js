5884d8358912ce2aefdf66ae549dbb47b2ab5525
Angelina Wedemeyer <angelina.wedemeyer@gmail.com>
2014-08-06 11:31:57 -0700

    separate

--- pictures.js
+++ pictures.js
@@ -1,25 +1,9 @@
-
-var main = function() {
-  $.ajax({
-    type: "GET",
-    dataType: "jsonp",
-    cache: false,
-    url: "https://api.instagram.com/v1/media/popular?access_token=48121533.5db54f4.df09ebad5d3d47d3979743c9c3b57b37",
-    success: function(data) {
-      for (var i = 0; i < 6; i++) {
-        $(".latest").append("<li>hi</li>");
-      }
-    }
-
-  });
-  function initialize() {
+function initialize() {
         var mapOptions = {
           center: new google.maps.LatLng(33.989854, -118.473859),
           zoom: 13
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
-};
-
-
-$(document).ready(main);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
