2d3badc6f8ea55a7e20594b9a5470de456777990
Angelina Wedemeyer <angelina.wedemeyer@gmail.com>
2014-08-06 11:02:05 -0700

    r

--- pictures.js
+++ pictures.js
@@ -12,7 +12,14 @@ var main = function() {
     }
 
   });
-}
+  function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(33.989854, -118.473859),
+          zoom: 13
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+};
 
 
 $(document).ready(main);
