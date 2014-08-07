49ab18d7cc732827b09b26cc5c9af74a14ed6b12
Angelina Wedemeyer <angelina.wedemeyer@gmail.com>
2014-08-06 17:49:55 -0700

    dfelete

--- pictures.js
+++ pictures.js
@@ -7,22 +7,3 @@ function initialize() {
             mapOptions);
       }
       google.maps.event.addDomListener(window, 'load', initialize);
-
-
-var main = function() {
-  $.ajax({
-    type: "GET",
-    dataType: "jsonp",
-    cache: false,
-    url: "https://api.instagram.com/v1/media/popular?access_token=48121533.5db54f4.df09ebad5d3d47d3979743c9c3b57b37",
-    success: function(data) {
-      for (var i = 0; i < 6; i++) {
-        $(".latest").append("<li>hi</li>");
-      }
-    }
-
-  });
-};
-
-
-$(document).ready(main);
