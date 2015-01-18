c1aa4105393b27cde245f3098e9fd628e2fcc86b
Aangelinaa <aewedemeyer@yahoo.com>
2015-01-18 12:00:07 -0800

    remove absolute

--- test1.js
+++ test1.js
@@ -77,7 +77,6 @@ $(document).ready(function() {
     };
 
 jQuery.fn.center = function () {
-    this.css("position","absolute");
     this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
                                                 $(window).scrollLeft()) + "px");
     return this;
