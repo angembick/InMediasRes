90bec46467fe40fc5fe947632d3666b79e632a1f
Aangelinaa <aewedemeyer@yahoo.com>
2015-01-18 11:56:57 -0800

    center function

--- test1.js
+++ test1.js
@@ -72,14 +72,18 @@ $(document).ready(function() {
         $('#countryTitle').empty()
         $('#countryTitle').append('<h1>'+countryObj.name+'</h1>');
         $('#countryTitle h1').fadeIn(1000);
-        $('#countryTitle').position({
-           my: "center",
-           at: "center",
-           of: window
-        });
+        $('#countryTitle').center();
       });
     };
 
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
 
 
       function populateBlogs(countryObj){ 
