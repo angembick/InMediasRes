837cabdcc4e168fcf81d282a5cf8b53009644f29
Aangelinaa <aewedemeyer@yahoo.com>
2015-01-19 13:44:50 -0800

    1000

--- projectE.js
+++ projectE.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 
 	function problem1(){
 		var total = 0;
-		for(var i = 0; i< 10; i++){
+		for(var i = 0; i< 1000; i++){
 			if ((i%3===0) || (i%5 ===0)){
 				total += i;
 			}
