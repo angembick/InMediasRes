a50ecae6266c5b10577f60a89a5a73697f23daa0
Aangelinaa <aewedemeyer@yahoo.com>
2015-01-20 14:32:52 -0800

    5.5

--- projectE.js
+++ projectE.js
@@ -112,7 +112,7 @@ $(document).ready(function(){
 
 
 	function problem5(){
-		var product = null;
+		var product = 'empty';
 		for(var i=1; product.isNan(); i++){
 			for(var j = 1; j<=10; j++){
 				if(i%j !==0){
