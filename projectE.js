4ff251761027536bdd37b01e8b7a369bf4201ddd
Aangelinaa <aewedemeyer@yahoo.com>
2015-01-20 14:37:02 -0800

    nan 5.6

--- projectE.js
+++ projectE.js
@@ -112,8 +112,8 @@ $(document).ready(function(){
 
 
 	function problem5(){
-		var product = null;
-		for(var i=1; product===null; i++){
+		var product = 'empty';
+		for(var i=1; isNan(product); i++){
 			for(var j = 1; j<=10; j++){
 				if(i%j !==0){
 					break;
