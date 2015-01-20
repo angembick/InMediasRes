ac2af052cd06c7e669425385e92381cc50105cf0
Aangelinaa <aewedemeyer@yahoo.com>
2015-01-20 14:34:17 -0800

    null 5.5

--- projectE.js
+++ projectE.js
@@ -112,8 +112,8 @@ $(document).ready(function(){
 
 
 	function problem5(){
-		var product = 'empty';
-		for(var i=1; product.isNan(); i++){
+		var product = null;
+		for(var i=1; product===null; i++){
 			for(var j = 1; j<=10; j++){
 				if(i%j !==0){
 					break;
