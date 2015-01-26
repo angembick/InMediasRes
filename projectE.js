89faac297f261ac53552e89fdf6e90ee2e95c49a
Aangelinaa <aewedemeyer@yahoo.com>
2015-01-25 21:40:33 -0800

    debug

--- projectE.js
+++ projectE.js
@@ -246,9 +246,11 @@ $(document).ready(function(){
 		alert(primeSum);
 	};
 
+	var row1 = ('08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08').slice(' ');
+
 	function problem11(){
 		var arrayOfRows = [
-		 [(('08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08').slice(' '))],
+		 [row1],
 		 [(('49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00').slice(' '))],
 		 [(('81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65').slice(' '))],
 		 [(('52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91').slice(' '))],
@@ -295,7 +297,7 @@ $(document).ready(function(){
 				}
 			}
 		}
-		alert(arrayOfRows[0][0]+' end '+arrayOfRows[19][19]);
+		alert(arrayOfRows[0][0]+' end '+arrayOfRows[19][0]);
 	};
 	problem11();
 
