6645c4622b044cf906bd407086f28f6052fbed02
Aangelinaa <aewedemeyer@yahoo.com>
2015-02-11 19:19:31 -0800

    object

--- test1.js
+++ test1.js
@@ -61,9 +61,9 @@ $(document).ready(function() {
        if(myTrip[i].name === countryID){
         if (displayCountry != myTrip[i]){
 
-          $('#'+displayCountry[0].toLowerCase()+'Button').removeClass('showCountry');
+          $('#'+displayCountry.name.toLowerCase()+'Button').removeClass('showCountry');
           displayCountry = myTrip[i];
-          $('#'+displayCountry[0].toLowerCase()+'Button').addClass('showCountry');
+          $('#'+displayCountry.name.toLowerCase()+'Button').addClass('showCountry');
 
           populateBlogs(myTrip[i]);
           updateCountryTitle(myTrip[i]);
