c64aed99192fb5f5c84ff227949fc4b84f6aa50b
Angelina Wedemeyer <angelinawedemeyer@Angelinas-MacBook.local>
2015-08-31 22:00:33 -0400

    initial commit

--- backup/map.js
+++
@@ -1,9 +0,0 @@
-function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(37.7423320, -122.4524330),
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
